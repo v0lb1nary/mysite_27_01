b02d1363188a8567c652e146c11105a4c15c2214
V0l_B1nary <volpatocursin@outlook.com>
2021-01-27 15:55:58 -0300

    set_form

--- blog/forms.py
+++ blog/forms.py
@@ -3,9 +3,7 @@ from .modlds import *
 
 class PostEmailForm(forms.ModelForm):
     """Form definition for PostEmail."""
-
-    class Meta:
-        """Meta definition for PostEmailform."""
-
-        model = PostEmail
-        fields = ('',)
+    nome = forms.CharField(max_length=30, required=False)
+    email = forms.EmailField(required=False)
+    para = forms.CharField(max_length=30, required=False)
+    comentario = forms.CharField(max_length=250, required=None)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Postagem
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import *
 
 def lista_postagem(request):
     lista_objetos = Postagem.publicado.all()
@@ -25,3 +26,22 @@ def detalhe_postagem(request, ano, mes, dia, rotulo):
                             data_publicacao__day = dia)
     return render(request, 'blog/postagem/detalhe.html', { 'post' : post})
 
+def compartilhar_postagem(request, post_id):
+    post = get_object_or_404(Postagem, id=post_id, status='publicado')
+    sent = False
+
+    if request.method == 'POST':
+        form = FormularioPostEmail(request.POST)
+
+        if form.is_valed():
+            cd = form.cleaned_data 
+            post_url = request.build_absolute_url(post.get_absolute_url())
+            assunto = f"{cd['nome']} recomendado que vc leia" f"{ post.titulo }"
+            mensagem = f"Leia {post.titulo} em {post.url}\n \n" f"{cd['nome']}\s comentario: " f"{cd['comentarios']}"  
+            send_email (assunto, mensagem, 'pyhton.django@gmail.com', [cd['para']])
+            sent = True
+    
+    else:
+        form = FormularioPostEmail()
+
+        return render(request, 'blog/postagem/compartilhar.html,' {'post':post, 'form':form, 'sent':sent})
