adfa42b7adb9fee156f165d3884b2ee7bf2209d6
V0l_B1nary <volpatocursin@outlook.com>
2021-01-27 20:47:14 -0300

    Debugado_Correct

--- blog/views.py
+++ blog/views.py
@@ -26,6 +26,7 @@ def detalhe_postagem(request, ano, mes, dia, rotulo):
                             data_publicacao__month = mes, 
                             data_publicacao__day = dia)
     
+
     #Lista dos comentários ativos para esta postagem
     comentarios = post.comentarios.filter(ativo=True)
     novo_comentario = None
@@ -41,23 +42,22 @@ def detalhe_postagem(request, ano, mes, dia, rotulo):
                                                             'comentarios': comentarios,
                                                             'novo_comentario': novo_comentario,
                                                             'formulario_comentario': formulario_comentario})
-                                                            
-def compartilhar_postagem(request, post_id):
+
+def compartilhar_postagens(request, post_id):
     post = get_object_or_404(Postagem, id=post_id, status='publicado')
     sent = False
 
     if request.method == 'POST':
         form = FormularioPostEmail(request.POST)
 
-        if form.is_valed():
+        if form.is_valid():
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url())
             assunto = f"{cd['nome']} recomendado que você leia" f"{ post.titulo }"
             mensagem = f"Leia {post.titulo} em {post_url}\n\n" f"{cd['nome']}\'s comentarios: {cd ['comentarios']}"
             send_mail(assunto, mensagem, 'pyhton.django@gmail.com', [cd['para']])
             sent = True
-    
     else:
         form = FormularioPostEmail()
-
-        return render(request, 'blog/postagem/compartilhar.html', {'post':post, 'form':form, 'sent':sent})
+    
+    return render(request, 'blog/postagem/compartilhar.html', {'post':post, 'form':form, 'sent':sent}) 
